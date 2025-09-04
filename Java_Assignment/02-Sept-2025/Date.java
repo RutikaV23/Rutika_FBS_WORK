d735a7880b53b73152d3a4aa2200f822a1ddb494
Rutika Vaidya <147318835+RutikaV23@users.noreply.github.com>
2025-09-04 12:14:59 +0530

    Add files via upload

---
+++ Java_Assignment/02-Sept-2025/Date.java
@@ -0,0 +1,20 @@
+class Date
+{
+	int day,month,year;
+}
+
+class Test 
+{
+	public static void main(String []args)
+	{
+		Date d1;
+                d1= new Date();
+		System.out.println(d1);
+
+		Date d2;
+                d2= new Date();
+		System.out.println(d2);
+
+		
+	}
+}
