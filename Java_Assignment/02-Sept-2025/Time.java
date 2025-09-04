16c01f3572c8625be495da9666efb6544102ac63
Rutika Vaidya <147318835+RutikaV23@users.noreply.github.com>
2025-09-04 12:20:57 +0530

    Add files via upload

---
+++ Java_Assignment/02-Sept-2025/Time.java
@@ -0,0 +1,19 @@
+class Time
+{
+	int sec,min,hour;
+}
+class Test
+{
+	public static void main(String[] args)
+	{
+		Time t1;
+		t1=new Time();	
+		System.out.println(t1);
+		
+
+		
+		Time t2;
+		t2=new Time();
+		System.out.println(t2);
+	}
+}
