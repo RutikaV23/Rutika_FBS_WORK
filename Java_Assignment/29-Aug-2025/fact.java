180e86fd47a787fb6aab351350def8ee64c2bf38
Rutika Vaidya <147318835+RutikaV23@users.noreply.github.com>
2025-09-04 12:07:31 +0530

    Add files via upload

---
+++ Java_Assignment/29-Aug-2025/fact.java
@@ -0,0 +1,16 @@
+class fact
+{
+	public static void main(String[] args)
+	{
+		
+	int sum = 1;
+	int n = 5;
+	while(n>0)
+	{
+	  sum = sum * n;
+	  n--;	
+	} 
+	System.out.println(sum);	
+
+	}
+}// class close here
---
+++ Java_Assignment/29-Aug-2025/test.java
@@ -0,0 +1,15 @@
+class test
+{
+	public static void main(String[] args)
+	{
+		System.out.println("hellow");
+	}
+}
+
+class test_1
+{
+	public static void main(String[] args)
+	{
+		System.out.println("World");
+	}
+}
