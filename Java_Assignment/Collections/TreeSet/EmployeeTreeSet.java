dafa3e4f3f5028c859982a5428e3d7e1ef19adb5
Rutika Vaidya <147318835+RutikaV23@users.noreply.github.com>
2025-10-11 23:21:46 +0530

    Add files via upload

---
+++ Java_Assignment/Collections/TreeSet/EmployeeTreeSet.java
@@ -0,0 +1,25 @@
+package list;
+
+import java.util.*;
+
+import p1.Employee;
+import p2.Admin;
+import p2.HR;
+import p2.SalesManger;
+
+public class EmployeeTreeSet 
+{
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		TreeSet employee = new TreeSet();
+		
+		employee.add(new Employee(101,"rutika",45000)); 
+		employee.add(new Admin(101,"rutika",45000,670));
+		employee.add(new HR(101,"rutika",45000,350));
+		employee.add(new SalesManger(101,"rutika",45000,230,4));
+		
+		System.out.println(employee);
+	}
+	
+}
