600bff7079980274ebd9a158252a2345a9c7609d
Rutika Vaidya <147318835+RutikaV23@users.noreply.github.com>
2025-10-11 23:17:27 +0530

    Add files via upload

---
+++ Java_Assignment/Collections/ArrayList/Test.java
@@ -0,0 +1,31 @@
+import java.util.*;
+public class Test 
+{
+
+	public static void main(String[] args) 
+	{
+//		ArrayList a1 = new ArrayList();
+//		LinkedList a1 = new LinkedList();
+//		a1.add(10);
+//		a1.add(20);
+//		a1.add(30);
+//		a1.add(40);
+//		a1.add(50);
+//		a1.add(60);
+//		
+//		System.out.println(a1);
+//		
+//		System.out.println(a1.get(4));
+//		
+//		System.out.println(a1.get(8));
+		
+		TreeSet t1 = new TreeSet();
+		t1.add(10);
+		t1.add(30);
+		t1.add(5);
+		t1.add(20);
+		
+		System.out.println(t1);
+	}
+
+}
